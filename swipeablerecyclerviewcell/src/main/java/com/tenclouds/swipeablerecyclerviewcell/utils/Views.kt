1ee70c9dfb280b071cd319947ae3bd18b01d8adf
Ryszard Śpiewak <ryszard.spiewak@10clouds.com>
2018-09-12 15:59:34 +0200

    variable renamed

--- swipeablerecyclerviewcell/src/main/java/com/tenclouds/swipeablerecyclerviewcell/utils/Views.kt
+++ swipeablerecyclerviewcell/src/main/java/com/tenclouds/swipeablerecyclerviewcell/utils/Views.kt
@@ -94,17 +94,17 @@ internal fun Canvas.drawConnector(movementProgress: Float,
 internal fun @receiver:ColorInt Int.blend(color: Int, ratio: Float): Int =
         ColorUtils.blendARGB(this, color, ratio)
 
-private val sNextGeneratedId = AtomicInteger(1)
+private val nextGeneratedId = AtomicInteger(1)
 fun View.generateViewId(): Int {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
         return View.generateViewId()
     } else {
         while (true) {
-            val result = sNextGeneratedId.get()
+            val result = nextGeneratedId.get()
             // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
             var newValue = result + 1
             if (newValue > 0x00FFFFFF) newValue = 1 // Roll over to 1, not 0.
-            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+            if (nextGeneratedId.compareAndSet(result, newValue)) {
                 return result
             }
         }
